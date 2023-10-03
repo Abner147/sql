1118f73f2a94bb4a7d77e97575a834042b5ba332
Abner147 <141387930+Abner147@users.noreply.github.com>
2023-10-02 21:17:17 -0600

    Add files via upload

---
+++ base de datos/base_de_datos_compras_detalle.sql
@@ -0,0 +1,57 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: localhost    Database: base_de_datos
+-- ------------------------------------------------------
+-- Server version	8.0.34
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `compras_detalle`
+--
+
+DROP TABLE IF EXISTS `compras_detalle`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `compras_detalle` (
+  `idCompras_detalle` bigint NOT NULL AUTO_INCREMENT,
+  `idCompras` int DEFAULT NULL,
+  `idProductos` int DEFAULT NULL,
+  `cantidad` int DEFAULT NULL,
+  `precio_costo` decimal(8,0) DEFAULT NULL,
+  PRIMARY KEY (`idCompras_detalle`),
+  KEY `idColmpras_compras_compras_detalles_idx` (`idCompras`),
+  KEY `idCompras_detalle_producto_compra_idx` (`idProductos`),
+  CONSTRAINT `idCompras_compras_compras_detalles` FOREIGN KEY (`idCompras`) REFERENCES `compras` (`idCompras`) ON UPDATE CASCADE,
+  CONSTRAINT `idCompras_detalle_producto_compra` FOREIGN KEY (`idProductos`) REFERENCES `productos` (`idProductos`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `compras_detalle`
+--
+
+LOCK TABLES `compras_detalle` WRITE;
+/*!40000 ALTER TABLE `compras_detalle` DISABLE KEYS */;
+/*!40000 ALTER TABLE `compras_detalle` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-10-02 21:13:05
---
+++ base de datos/base_de_datos_empleados.sql
@@ -0,0 +1,62 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: localhost    Database: base_de_datos
+-- ------------------------------------------------------
+-- Server version	8.0.34
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `empleados`
+--
+
+DROP TABLE IF EXISTS `empleados`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `empleados` (
+  `idEmpleados` int NOT NULL AUTO_INCREMENT,
+  `nombres` varchar(60) DEFAULT NULL,
+  `apellidos` varchar(60) DEFAULT NULL,
+  `direccion` varchar(80) DEFAULT NULL,
+  `telefono` varchar(25) DEFAULT NULL,
+  `DPI` varchar(15) DEFAULT NULL,
+  `genero` bit(1) DEFAULT NULL,
+  `fecha_nacimiento` date DEFAULT NULL,
+  `idPuestos` smallint DEFAULT NULL,
+  `fecha_inicio_labores` date DEFAULT NULL,
+  `fecha_ingreso` datetime DEFAULT NULL,
+  PRIMARY KEY (`idEmpleados`),
+  KEY `idPuestos_puestos_empleados_idx` (`idPuestos`),
+  CONSTRAINT `idPuestos_puestos_empleados` FOREIGN KEY (`idPuestos`) REFERENCES `puestos` (`idPuestos`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `empleados`
+--
+
+LOCK TABLES `empleados` WRITE;
+/*!40000 ALTER TABLE `empleados` DISABLE KEYS */;
+INSERT INTO `empleados` VALUES (1,'edin','esquit','avenida','5555555','555555555',_binary '\0','0200-05-05',5,'2000-05-05','2000-05-05 12:05:01'),(2,'edin','esquit','avenida','5555555','555555555',_binary '\0','0200-05-05',5,'2000-05-05','2000-05-05 12:05:01'),(3,'ramon','esquit','avenida','555','888',NULL,'2000-08-08',4,'2555-09-09','2000-05-09 00:00:00');
+/*!40000 ALTER TABLE `empleados` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-10-02 21:13:04
